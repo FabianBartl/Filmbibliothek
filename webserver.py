b5dac6feb81574cae0980a28466d5446c4a41d49
FabianBartl <fabian@informatic-freak.de>
2023-01-08 16:42:25 +0100

    add truncate filter

--- webserver.py
+++ webserver.py
@@ -5,15 +5,26 @@ from urllib.parse import unquote_plus, quote_plus
 from flask import Flask
 from flask import redirect, url_for, render_template, send_from_directory
 
-# init flask app and add custom jinja filters 
+# init flask app
 app = Flask(__name__)
-app.jinja_env.filters["urlEncode"] = lambda value: quote_plus(value)
 
 # load movies data file into global variable
 with open(os.path.join("static", "movies.json"), "r", encoding="utf-8") as file:
 	movies_json = json.load(file)
 
-# set environment variables for useage in templates
+# ---------- custom jinja filters ----------
+app.jinja_env.filters["urlEncode"] = lambda value: quote_plus(value)
+
+# truncate string and append ellipsis
+def truncate(value, length, ellipsis="..."):
+	if len(value+ellipsis) > length:
+		return value[:length-len(ellipsis)] + ellipsis
+	return value
+app.jinja_env.filters["truncate"] = truncate
+
+# ---------- jinja context variable ----------
+
+# set context variables for useage in templates
 @app.context_processor
 def utility_processor():
 	return {
