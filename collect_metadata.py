4d9955146f1c3f12d2a45a0cda82eb0235443921
FabianBartl <fabian@informatic-freak.de>
2023-01-11 22:00:35 +0100

    clean up data collector

--- collect_metadata.py
+++ collect_metadata.py
@@ -5,58 +5,50 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 # readout metadata file and store it as dictionary
-def parseMetadataFromFile(filename:str) -> dict:
+def parseMetadataFromFile(filename:str, only_description:bool=True) -> dict:
 	metadata = {}
 
 	with open(filename, "r", encoding="utf-8") as file:
-		metadata["channel"] = file.readline()
-		metadata["topic"] = file.readline()
-		file.readline()
-		metadata["title"] = file.readline()
-		file.readline()
-		metadata["date"] = file.readline()
-		metadata["time"] = file.readline()
-		metadata["duration"] = file.readline()
-		metadata["filesize"] = file.readline()
-		file.readline()
-		file.readline()
+		if not only_description:
+			metadata["channel"] = file.readline()
+			metadata["topic"] = file.readline()
+			file.readline()
+			metadata["title"] = file.readline()
+			file.readline()
+			metadata["date"] = file.readline()
+			metadata["time"] = file.readline()
+			metadata["duration"] = file.readline()
+			metadata["filesize"] = file.readline()
+			file.readline()
+			file.readline()
 
-		for param in metadata:
-			metadata[param] = metadata[param].strip()[13:]
+			for param in metadata:
+				metadata[param] = metadata[param].strip()[13:]
 
-		metadata["website"] = file.readline().strip()
-		file.readline()
-		file.readline()
-		metadata["url"] = file.readline().strip()
-		file.readline()
+			metadata["website"] = file.readline().strip()
+			file.readline()
+			file.readline()
+			metadata["url"] = file.readline().strip()
+			file.readline()
 
-		beschreibung = []
+		else:
+			for _ in range(16):
+				file.readline()
+
+		description = []
 		while line := file.readline():
 			if line.strip() == "": continue
-			beschreibung.append(line.strip())
-		metadata["description"] = " ".join(beschreibung)
+			description.append(line.strip())
+		metadata["description"] = " ".join(description)
 
 	return metadata
 
 # get movie poster as url
-def getMoviePoster(moviename:str, source:str="imdb", ignoreError=False) -> str:
+def getMoviePoster(moviename:str, ignoreError:bool=False) -> str:
 	relImgNotFound = Exception("Related image not found")
-	header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,"referer":"https://www.google.com/"}
-	
-	movienameURL = urllib.parse.quote_plus(moviename)
-	if source == "amazon":
-		requestURL = "https://www.amazon.de/s?k=" + movienameURL
-	elif source == "bing":
-		requestURL = "https://www.bing.com/images/search?q=" + movienameURL + "&qft=+filterui:aspect-tall&first=1&tsc=ImageHoverTitle&cw=1177&ch=705"
-	elif source == "google":
-		pass
-	elif source == "ebay":
-		pass
-	elif source == "imdb":
-		requestURL = "https://www.imdb.com/find/?q=" + movienameURL
-	else:
-		raise ValueError(f"unknow source: {source}")
 
+	header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,"referer":"https://www.google.com/"}
+	requestURL = "https://www.imdb.com/find/?q=" + urllib.parse.quote_plus(moviename)
 	request = requests.get(requestURL, headers=header)
 	if request.status_code != 200:
 		if ignoreError:
@@ -64,61 +56,31 @@ def getMoviePoster(moviename:str, source:str="imdb", ignoreError=False) -> str:
 		raise Exception(f"HTTP response code: {request.status_code}")
 	html = BeautifulSoup(request.content, features="html.parser")
 
-	if source == "amazon":
-		img = html.find("img", attrs={"alt": moviename})
-		if img is None:
-			if ignoreError:
-				return ""
-			raise relImgNotFound
-		return img.get("data-src")
+	page = html.find("a", attrs={"class": "ipc-metadata-list-summary-item__t"})
+	if page is None:
+		if ignoreError:
+			return ""
+		raise relImgNotFound
+	pageURL = "https://www.imdb.com" + page.get("href")
+
+	request = requests.get(pageURL, headers=header)
+	if request.status_code != 200:
+		if ignoreError:
+			return ""
+		raise Exception(f"HTTP response code: {request.status_code}")
+	html = BeautifulSoup(request.content, features="html.parser")
 	
-	elif source == "bing":
-		imgs = [ tag for tag in html.findAll(attrs={"class": "iusc"}) if tag.get("class") == ["iusc"] ]
-		if imgs == []:
-			if ignoreError:
-				return ""
-			raise relImgNotFound
-
-		imgURL = [ param.replace("mediaurl=", "") for param in imgs[0].get("href").split("&") if param.startswith("mediaurl=") ]
-		if imgURL == []:
-			if ignoreError:
-				return ""
-			raise relImgNotFound
-		return urllib.parse.unquote(imgURL[0])
-
-	elif source == "google":
-		pass
-
-	elif source == "ebay":
-		pass
-
-	elif source == "imdb":
-		page = html.find("a", attrs={"class": "ipc-metadata-list-summary-item__t"})
-		if page is None:
-			if ignoreError:
-				return ""
-			raise relImgNotFound
-		pageURL = "https://www.imdb.com" + page.get("href")
-
-		request = requests.get(pageURL, headers=header)
-		if request.status_code != 200:
-			if ignoreError:
-				return ""
-			raise Exception(f"HTTP response code: {request.status_code}")
-		html = BeautifulSoup(request.content, features="html.parser")
-		
-		imgs = [ tag for tag in html.findAll("img", attrs={"class": "ipc-image"}) if tag.get("class") == ["ipc-image"] ]
-		if imgs == []:
-			if ignoreError:
-				return ""
-			raise relImgNotFound
-		return imgs[0].get("srcset").split(", ")[-1].split(" ")[0]
+	imgs = [ tag for tag in html.findAll("img", attrs={"class": "ipc-image"}) if tag.get("class") == ["ipc-image"] ]
+	if imgs == []:
+		if ignoreError:
+			return ""
+		raise relImgNotFound
+	return imgs[0].get("srcset").split(", ")[-1].split(" ")[0]
 
 # get metadata from imdb
-def getMetadataFromIMDB(moviename:str, ignoreError=False) -> dict:
+def getMetadataFromIMDB(moviename:str, ignoreError:bool=False) -> dict:
 	header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,"referer":"https://www.google.com/"}
-
-	requestURL = "https://www.imdb.com/find/?q="+urllib.parse.quote_plus(moviename)
+	requestURL = "https://www.imdb.com/find/?q=" + urllib.parse.quote_plus(moviename)
 	request = requests.get(requestURL, headers=header)
 	if request.status_code != 200:
 		if ignoreError:
@@ -134,18 +96,9 @@ def getMetadataFromIMDB(moviename:str, ignoreError=False) -> dict:
 	
 	metadata = {}
 	metadata["imdb_id"] = page.get("href").split("/")[2]
-	metadata["imdb_url"] = f"https://www.imdb.com/title/" + metadata["imdb_id"]
+	metadata["imdb_url"] = "https://www.imdb.com/title/" + metadata["imdb_id"]
 	metadata["poster"] = getMoviePoster(moviename, "imdb")
 
-	requestURL = metadata["imdb_url"] + "/fullcredits/"
-	request = requests.get(requestURL, headers=header)
-	if request.status_code != 200:
-		if ignoreError:
-			return {}
-		raise Exception(f"HTTP response code: {request.status_code}")
-	html = BeautifulSoup(request.content, features="html.parser")
-
-	metadata["credits_html"] = html.find(id="fullcredits_content").__repr__()
 	return metadata
 
 # save collected metadata as json format
@@ -177,7 +130,7 @@ def run(movie_directory):
 		metadata[movieID] = movie_metadata
 		movieID += 1
 	
-	saveMetadata(os.path.join("static", "movies.json"), metadata)
+	saveMetadata(os.path.join("static", "data", "movies.json"), metadata)
 
 # cli
 if __name__ == "__main__":
