d669542bb6d45f48c5a3920aa6a670d2827f9f14
FabianBartl <fabian@informatic-freak.de>
2023-01-17 18:44:05 +0100

    use CAPS for global vars; remove index post actions; some renames

--- webserver.py
+++ webserver.py
@@ -7,40 +7,28 @@ from flask import redirect, url_for, render_template, send_from_directory, abort
 
 # ---------- global used variables ----------
 
-movies_json = {}
-config_yaml = {}
-debug_mode = False
+MOVIES = {}
+CONFIG = {}
+DEBUG = False
+
+# init flask app
+app = Flask(__name__)
 
 # ---------- functions ----------
 
 # load movies data file into global variable
 def load_movies() -> dict:
 	with open(os.path.join("static", "data", "movies.json"), "r", encoding="utf-8") as file:
-		global movies_json
-		movies_json = json.load(file)
-		return movies_json
+		return json.load(file)
 
 # load config from yaml file
-def load_config():
+def load_config() -> dict:
 	with open("config.yml", "r", encoding="utf-8") as file:
-		global config_yaml
-		config_yaml = yaml.safe_load(file)
-		return config_yaml
-
-# search for software updates
-def update_software():
-	pass
-
-# collect metadata and reload movie data
-def collect_metadata():
-	pass
-
-# init flask app
-app = Flask(__name__)
+		return yaml.safe_load(file)
 
 # ---------- custom jinja filters ----------
 
-app.jinja_env.filters["urlEncode"] = lambda value: quote_plus(value)
+app.jinja_env.filters["urlEncode"] = lambda url: quote_plus(url)
 
 # truncate string and append ellipsis
 def truncate(value, length, ellipsis="..."):
@@ -54,8 +42,8 @@ app.jinja_env.filters["truncate"] = truncate
 # set context variables for useage in templates
 @app.context_processor
 def inject_variables():
-	global config_yaml
-	return {"config": config_yaml}
+	global CONFIG
+	return {"config": CONFIG}
 
 # ---------- error pages ----------
 
@@ -67,50 +55,43 @@ def not_found(error):
 
 # movie overview
 @app.route("/")
-@app.route("/", methods=["POST"])
 def index():
-	# post actions
-	if request.form.get("update_software"):
-		update_software()
-	if request.form.get("reload_movies"):
-		load_movies()
-	if request.form.get("collect_metadata"):
-		collect_metadata()
-	# return page
-	global movies_json, debug_mode
-	return render_template("index.html", movies=movies_json, debug_mode=debug_mode)
+	global MOVIES, DEBUG
+	return render_template("index.html", movies=MOVIES, debug_mode=DEBUG)
 
 # detailed movie data
 @app.route("/movie/<movieID>/")
 def movie(movieID):
-	global movies_json, debug_mode
-	if movie := movies_json.get(movieID):
-		return render_template("movie.html", movie=movie, debug_mode=debug_mode)
+	global MOVIES, DEBUG
+	if movie := MOVIES.get(movieID):
+		return render_template("movie.html", movie=movie, debug_mode=DEBUG)
 	return abort(404)
 
 # stream movie
 @app.route("/movie/<movieID>/stream/")
 def movie_stream(movieID):
-	global movies_json
-	if movie_data := movies_json.get(movieID):
-		return send_from_directory(movie_data["directory"], f'{movie_data["filename"]}.{movie_data["extension"]}', as_attachment=False)
+	global MOVIES
+	if movie := MOVIES.get(movieID):
+		return send_from_directory(movie["directory"], f'{movie["filename"]}.{movie["extension"]}', as_attachment=False)
 	return abort(404)
 
 # get subtitles
 @app.route("/movie/<movieID>/subtitles/<language>/")
 def movie_subtitles_language(movieID, language):
-	global movies_json
-	if movie_data := movies_json.get(movieID):
-		if os.path.isfile(os.path.join(movie_data["directory"], file := f'{movie_data["filename"]}.{language}.vtt')):
-			return send_from_directory(movie_data["directory"], file, as_attachment=False)
+	global MOVIES
+	if movie := MOVIES.get(movieID):
+		if os.path.isfile(os.path.join(movie["directory"], file := f'{movie["filename"]}.{language}.vtt')):
+			return send_from_directory(movie["directory"], file, as_attachment=False)
 	return abort(404)
+
+
 
 # run webserver
 if __name__ == "__main__":
-	load_movies()
-	load_config()
+	MOVIES = load_movies()
+	CONFIG = load_config()
 
-	port = config_yaml.get("server_port", 80)
-	debug_mode = config_yaml.get("debug_mode", False)
+	DEBUG = CONFIG.get("debug_mode", False)
+	port = CONFIG.get("server_port", 80)
 	
 	app.run(debug=True, port=port)
