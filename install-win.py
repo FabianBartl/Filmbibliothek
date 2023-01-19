6a018c59bde8066217c5b96ec84404d140e12988
FabianBartl <fabian@informatic-freak.de>
2023-01-19 01:19:59 +0100

    Update install-win.py

--- install-win.py
+++ install-win.py
@@ -2,45 +2,49 @@
 import os, sys, yaml, subprocess
 
 # install python packages
-with open("requirements.txt", "r", encoding="utf-8") as file:
-	requirements = [ line.strip() for line in file.readlines() ]
-for package in requirements:
-	if not package in sys.modules:
-		subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+try:
+    import winshell, collect_metadata
+    from win32com.client import Dispatch
+    from colorama import Fore, Back, Style, init
+    
+    init(autoreset=True)
+    print(Fore.GREEN + "Packages already installed")
 
-# these packages are first available after the requirements.txt installation
-import winshell, collect_metadata
-from win32com.client import Dispatch
-from colorama import Fore, Back, Style, init
-init(autoreset=True)
+except:
+    with open("requirements.txt", "r", encoding="utf-8") as file:
+        requirements = [ line.strip() for line in file.readlines() ]
+    for package in requirements:
+        if not package in sys.modules:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-print(Fore.GREEN + "Packages installed\n")
+    # these packages are first available after the requirements.txt installation
+    import winshell, collect_metadata
+    from win32com.client import Dispatch
+    from colorama import Fore, Back, Style, init
+    
+    init(autoreset=True)
+    print(Fore.GREEN + "Packages installed")
 
-# add host
-host = "filmbibliothek"
-with open("C:\\Windows\\System32\\drivers\\etc\\hosts", "r", encoding="utf-8") as file:
-    content = file.read()
-with open("C:\\Windows\\System32\\drivers\\etc\\hosts", "a", encoding="utf-8") as file:
-    if not host in content:
-        file.write(f"\n# host for flask webserver of '{host}' project")
-        file.write(f"\n127.0.0.1    {host}\n")
-
-print(Fore.GREEN + f"Host '{host}' added to DNS resolver\n")
-
-# create movies.json and store movie_directory in config.yml
-while not os.path.isdir(movie_directory := input("Movie directory: ")):
-    print(Fore.RED + "Directory not found\n")
 # load config.yml
 with open("config.yml", "r", encoding="utf-8") as file:
     config_yaml = yaml.safe_load(file)
-    config_yaml["movie-directories"] = [os.path.abspath(movie_directory)]
-# store movie_directory in config.yml
-with open("config.yml", "w+", encoding="utf-8") as file:
-    yaml.dump(config_yaml, file)
-# collect metadata
-collect_metadata.run(config_yaml["movie-directories"])
 
-print(Fore.GREEN + "Movie data collected and stored\n")
+# add host
+host = config_yaml.get("server-host", "filmbibliothek")
+customHost = False
+try:
+    with open("C:\\Windows\\System32\\drivers\\etc\\hosts", "r", encoding="utf-8") as file:
+        content = file.read()
+    with open("C:\\Windows\\System32\\drivers\\etc\\hosts", "a", encoding="utf-8") as file:
+        if not host in content:
+            file.write(f"\n# host for flask webserver of '{host}' project")
+            file.write(f"\n127.0.0.1    {host}\n")
+
+    customHost = True
+    print(Fore.GREEN + f"Host '{host}' added to DNS resolver")
+
+except PermissionError:
+    print(Fore.YELLOW + f"Run the installation script as administrator to add '{host}' as host to DNS resolver")
 
 # add webserver shortcut to startup
 shell = Dispatch("WScript.shell")
@@ -49,10 +53,34 @@ shortcut.Targetpath = os.path.abspath(os.path.join(os.path.curdir, "webserver.py
 shortcut.WorkingDirectory = os.path.abspath(os.path.curdir)
 shortcut.save()
 
-print(Fore.GREEN + "Webserver added to windows startup directory\n")
+print(Fore.GREEN + "Webserver script added to windows startup directory")
+
+# create movies.json and load movie directory from config.yml
+movie_directories = config_yaml.get("movie-directories", [])
+if movie_directories == [] or movie_directories == "" or movie_directories == None:
+    print(Fore.RED + f"No directories configured")
+    print("Please add your movie directories to the config.yml file. Then run the installation script again.")
+    input(Style.DIM + "Press Enter to close ...")
+    exit()
+    
+if not type(movie_directories) is list:
+    movie_directories = [ movie_directories ]
+
+for movie_directory in movie_directories:
+    if not os.path.isdir(str(movie_directory)):
+        print(Fore.RED + f"Directory '{movie_directory}' not found")
+        print("Please use valid absolute paths as movie directory. Then run the installation script again.")
+        input(Style.DIM + "Press Enter to close ...")
+        exit()
+
+# collect metadata
+collect_metadata.run(movie_directories)
+
+print(Fore.GREEN + "Movie data collected and stored")
 
 # start webserver via system restart
-print(Fore.YELLOW + "Please restart your system\n")
-
-# TODO: add client shortcut to desktop
-print(Fore.YELLOW + f"After restarting, the webpage will be available at: http://localhost/ or http://{host}/\n")
+port = config_yaml.get("server-port", 80)
+if port == 80:
+    print(Fore.YELLOW+"After restarting, the webpage will be available at: http://localhost/", Fore.YELLOW+"or "+Fore.GREEN+f"http://{host}/" if customHost else "")
+else:
+    print(Fore.YELLOW+f"After restarting, the webpage will be available at: localhost:{port}/", Fore.YELLOW+"or "+Fore.GREEN+f"{host}:{port}/" if customHost else "")
