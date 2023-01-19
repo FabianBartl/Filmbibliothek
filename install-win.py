fcd91a503f2a90349110ae1f287b4c1158ca26cc
FabianBartl <fabian@informatic-freak.de>
2023-01-19 14:56:55 +0100

    start webserver.py at the end

--- install-win.py
+++ install-win.py
@@ -30,10 +30,9 @@ try:
         if not host in content:
             file.write(f"\n# host for flask webserver of '{host}' project")
             file.write(f"\n127.0.0.1    {host}\n")
-
+    
     customHost = True
     print(Fore.GREEN + f"Host '{host}' added to DNS resolver")
-
 except PermissionError:
     print(Fore.YELLOW + f"Run the installation script as administrator to add '{host}' as host to DNS resolver")
 
@@ -43,14 +42,13 @@ shortcut = shell.CreateShortCut(os.path.join(winshell.startup(), f"webserver_{ho
 shortcut.Targetpath = os.path.abspath(os.path.join(os.path.curdir, "webserver.py"))
 shortcut.WorkingDirectory = os.path.abspath(os.path.curdir)
 shortcut.save()
-
 print(Fore.GREEN + "Webserver script added to windows startup directory")
 
 # create movies.json and load movie directory from config.yml
 movie_directories = config_yaml.get("movie-directories", [])
 if movie_directories == [] or movie_directories == "" or movie_directories == None:
     print(Fore.RED + f"No directories configured")
-    print("Please add your movie directories to the config.yml file. Then run the installation script again.")
+    print(Fore.YELLOW + "Please add your movie directories to the config.yml file. Then run the installation script again.")
     input(Style.DIM + "Press Enter to close ...")
     exit()
     
@@ -60,18 +58,21 @@ if not type(movie_directories) is list:
 for movie_directory in movie_directories:
     if not os.path.isdir(str(movie_directory)):
         print(Fore.RED + f"Directory '{movie_directory}' not found")
-        print("Please use valid absolute paths as movie directory. Then run the installation script again.")
+        print(Fore.YELLOW + "Please use valid absolute paths as movie directory. Then run the installation script again.")
         input(Style.DIM + "Press Enter to close ...")
         exit()
 
 # collect metadata
 collect_metadata.run(movie_directories)
-
 print(Fore.GREEN + "Movie data collected and stored")
 
-# start webserver via system restart
+# start webserver
 port = config_yaml.get("server-port", 80)
-if port == 80:
-    print(Fore.YELLOW+"After restarting, the webpage will be available at: http://localhost/", Fore.YELLOW+"or "+Fore.GREEN+f"http://{host}/" if customHost else "")
-else:
-    print(Fore.YELLOW+f"After restarting, the webpage will be available at: localhost:{port}/", Fore.YELLOW+"or "+Fore.GREEN+f"{host}:{port}/" if customHost else "")
+print(
+    Fore.GREEN + "The webpage will be available at:",
+    Fore.GREEN + f"http://localhost{':'+port if port!=80 else ''}/",
+    Fore.GREEN + "or " + Fore.GREEN+f"http://{host}{':'+port if port!=80 else ''}/" if customHost else ""
+)
+
+print("Run webserver.py")
+subprocess.check_call([sys.executable, "webserver.py"])
