9562f3a4160ad5ad2e1df8dd71afcc4c6ecf4f0b
FabianBartl <fabian@informatic-freak.de>
2023-01-24 01:43:18 +0100

    use tab indents

--- custom_logger.py
+++ custom_logger.py
@@ -3,24 +3,24 @@ import logging, os
 from datetime import datetime
 
 def init(name, *, name_is_path:bool=True):
-    # extract filename from path
-    if name_is_path:
-        name = os.path.basename(name)
+	# extract filename from path
+	if name_is_path:
+		name = os.path.basename(name)
 
-    # logging parameter
-    level = logging.DEBUG
-    timestamp = datetime.today().strftime("%Y-%m-%d_%H-%M-%S")
-    path = os.path.abspath(os.path.join("logs", f"{name}_{timestamp}.log"))
-    encoding = "utf-8"
-    format = "%(asctime)s | %(levelname)8s | %(message)s"
-    datefmt = "%Y-%m-%d %H:%M:%S"
+	# logging parameter
+	level = logging.DEBUG
+	timestamp = datetime.today().strftime("%Y-%m-%d_%H-%M-%S")
+	path = os.path.abspath(os.path.join("logs", f"{name}_{timestamp}.log"))
+	encoding = "utf-8"
+	format = "%(asctime)s | %(levelname)8s | %(message)s"
+	datefmt = "%Y-%m-%d %H:%M:%S"
 
-    # file hanlder for logging
-    file_handler = logging.FileHandler(path, encoding=encoding)
-    file_handler.setLevel(level)
-    file_handler.setFormatter(logging.Formatter(fmt=format, datefmt=datefmt))
+	# file hanlder for logging
+	file_handler = logging.FileHandler(path, encoding=encoding)
+	file_handler.setLevel(level)
+	file_handler.setFormatter(logging.Formatter(fmt=format, datefmt=datefmt))
 
-    # logging config
-    logging.basicConfig(level=level, encoding=encoding, format=format, datefmt=datefmt, handlers=[file_handler])
+	# logging config
+	logging.basicConfig(level=level, encoding=encoding, format=format, datefmt=datefmt, handlers=[file_handler])
 
-    return logging.getLogger(name)
+	return logging.getLogger(name)
--- install-win.py
+++ install-win.py
@@ -6,11 +6,11 @@ logger.debug(f"start of script: {__file__}")
 # install python packages
 logger.debug("install requirements")
 with open("requirements.txt", "r", encoding="utf-8") as file:
-    requirements = [ line.strip() for line in file.readlines() ]
+	requirements = [ line.strip() for line in file.readlines() ]
 for package in requirements:
-    if not package in sys.modules:
-        logger.info(f"install {package=}")
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+	if not package in sys.modules:
+		logger.info(f"install {package=}")
+		subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 # these packages are first available after the requirements.txt installation
 import winshell, collect_metadata, yaml
@@ -23,31 +23,31 @@ print(Fore.GREEN + "Packages installed")
 # load config.yml
 logger.debug("load config.yml")
 with open("config.yml", "r", encoding="utf-8") as file:
-    config_yaml = yaml.safe_load(file)
-    logger.info("config.yml loaded")
+	config_yaml = yaml.safe_load(file)
+	logger.info("config.yml loaded")
 
 # add host
 logger.debug("try: add host to local dns resolver")
 host = config_yaml.get("server-host", "filmbibliothek")
 customHost = False
 try:
-    with open("C:\\Windows\\System32\\drivers\\etc\\hosts", "r", encoding="utf-8") as file:
-        content = file.read()
-    with open("C:\\Windows\\System32\\drivers\\etc\\hosts", "a", encoding="utf-8") as file:
-        if not host in content:
-            file.write(f"\n# host for flask webserver of '{host}' project")
-            file.write(f"\n127.0.0.1    {host}\n")
-    
-    customHost = True
-    logger.info(f"added {host=} to local dns resolver")
-    print(Fore.GREEN + f"Host '{host}' added to DNS resolver")
+	with open("C:\\Windows\\System32\\drivers\\etc\\hosts", "r", encoding="utf-8") as file:
+		content = file.read()
+	with open("C:\\Windows\\System32\\drivers\\etc\\hosts", "a", encoding="utf-8") as file:
+		if not host in content:
+			file.write(f"\n# host for flask webserver of '{host}' project")
+			file.write(f"\n127.0.0.1    {host}\n")
+	
+	customHost = True
+	logger.info(f"added {host=} to local dns resolver")
+	print(Fore.GREEN + f"Host '{host}' added to DNS resolver")
 except PermissionError:
-    logger.warning(f"PermissionError: failed to add {host=} to local dns resolver")
-    print(Fore.YELLOW + f"Run the installation script as administrator to add '{host}' as host to local DNS resolver")
+	logger.warning(f"PermissionError: failed to add {host=} to local dns resolver")
+	print(Fore.YELLOW + f"Run the installation script as administrator to add '{host}' as host to local DNS resolver")
 except Exception as error:
-    logger.critical(error)
-    print(error)
-    exit()
+	logger.critical(error)
+	print(error)
+	exit()
 
 # add webserver.py to startup directory
 logger.debug("add webserver.py to windows startup directory")
@@ -68,25 +68,25 @@ print(Fore.GREEN + "Webserver script added to windows startup directory")
 logger.debug(f"load movie directories from config.yml")
 movie_directories = config_yaml.get("movie-directories", [])
 if movie_directories == [] or movie_directories == "" or movie_directories == None:
-    logger.error("no movie directories configured")
-    print(Fore.RED + f"No directories configured")
-    print(Fore.YELLOW + "Please add your movie directories to the config.yml file. Then run the installation script again.")
-    input(Style.DIM + "Press Enter to close ...")
-    exit()
-    
+	logger.error("no movie directories configured")
+	print(Fore.RED + f"No directories configured")
+	print(Fore.YELLOW + "Please add your movie directories to the config.yml file. Then run the installation script again.")
+	input(Style.DIM + "Press Enter to close ...")
+	exit()
+	
 if not type(movie_directories) is list:
-    logger.debug(f"convert single movie directory string to list: {movie_directories=}")
-    movie_directories = [ movie_directories ]
-    logger.debug(f"as list: {movie_directories=}")
+	logger.debug(f"convert single movie directory string to list: {movie_directories=}")
+	movie_directories = [ movie_directories ]
+	logger.debug(f"as list: {movie_directories=}")
 
 logger.debug("check validity of movie directories")
 for movie_directory in movie_directories:
-    if not os.path.isdir(str(movie_directory)):
-        logger.error(f"directory {movie_directory=} not found")
-        print(Fore.RED + f"Directory '{movie_directory}' not found")
-        print(Fore.YELLOW + "Please use valid absolute paths as movie directories inside the config.yml file. Then run the installation script again.")
-        input(Style.DIM + "Press Enter to close ...")
-        exit()
+	if not os.path.isdir(str(movie_directory)):
+		logger.error(f"directory {movie_directory=} not found")
+		print(Fore.RED + f"Directory '{movie_directory}' not found")
+		print(Fore.YELLOW + "Please use valid absolute paths as movie directories inside the config.yml file. Then run the installation script again.")
+		input(Style.DIM + "Press Enter to close ...")
+		exit()
 
 # collect metadata
 logger.debug(f"run collect_metadata.py with {movie_directories=}")
@@ -98,9 +98,9 @@ print(Fore.GREEN + "Movie data collected and stored")
 port = config_yaml.get("server-port", 80)
 logger.debug(f"show webserver address: {host=} {port=}")
 print(
-    Fore.GREEN + "The webpage will be available at:",
-    Fore.GREEN + f"http://localhost{':'+port if port!=80 else ''}/",
-    Fore.GREEN + "or " + Fore.GREEN+f"http://{host}{':'+port if port!=80 else ''}/" if customHost else ""
+	Fore.GREEN + "The webpage will be available at:",
+	Fore.GREEN + f"http://localhost{':'+port if port!=80 else ''}/",
+	Fore.GREEN + "or " + Fore.GREEN+f"http://{host}{':'+port if port!=80 else ''}/" if customHost else ""
 )
 
 logger.debug("run webserver.py")
