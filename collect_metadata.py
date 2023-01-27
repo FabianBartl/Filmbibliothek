f98435649d0e8c896a6029666523c043c4e2c264
FabianBartl <fabian@informatic-freak.de>
2023-01-27 02:17:34 +0100

    add getMetadatFromRottenTomatoes() function template

--- collect_metadata.py
+++ collect_metadata.py
@@ -46,6 +46,12 @@ def getMetadatFromMoviepilot(moviename:str) -> dict:
 	pass
 
 
+# get metadata from Rotten Tomatoes
+# https://www.rottentomatoes.com/m/23
+def getMetadatFromRottenTomatoes(moviename:str) -> dict:
+	pass
+
+
 # get metadata from imdb
 def getMetadataFromIMDB(moviename:str, *, imdb_id:str=None) -> dict:
 	header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36", "referer":"https://www.google.com/"}
