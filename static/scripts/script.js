e0ebe20dd5d90e1c8be8e211e351a544dab94c2d
FabianBartl <fabian@informatic-freak.de>
2023-01-28 18:43:40 +0100

    typos

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,15 +1,14 @@
 
 $(document).ready(function(){
-	// justify lenghts to length of widest element 
+	// justify lenghts to length of widest element
 	creditsName_maxWidth = 0;
 	$('.credit-name').each(function(){
 		creditsName_maxWidth = max($(this).width(), creditsName_maxWidth);
 	});
 	$('.credit-name').each(function(){
-		$(this).width(creditsName_maxWidth + 8);
+		$(this).width(creditsName_maxWidth);
 	});
 });
-
 
 // scroll-to video, make fullscreen, play it
 function playAsFullscreen(selector) {
