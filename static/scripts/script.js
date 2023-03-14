5f421e5e11c6733b3467f9a89a589b86fcc60711
FabianBartl <fabian@informatic-freak.de>
2023-03-14 22:50:56 +0100

    reduce video functions to accept video instead of video-wrapper as parameter #12

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -4,7 +4,7 @@ function max(a, b) { return a > b ? a : b; }
 function min(a, b) { return a < b ? a : b; }
 
 // cookies
-function setCookie(cookieName, cookieValue, expiredTime=1, unit="h") {
+function setCookie(cookieName, cookieValue, expiredTime = 1, unit = "h") {
 	const time = new Date();
 	var factor = 1;
 	switch (unit) {
@@ -30,7 +30,7 @@ function setCookie(cookieName, cookieValue, expiredTime=1, unit="h") {
 function getCookie(cookieName) {
 	var name = cookieName + "=";
 	var cookieArray = document.cookie.split(";");
-	for (var i=0; i<cookieArray.length; i++) {
+	for (var i = 0; i < cookieArray.length; i++) {
 		var cookieChar = cookieArray[i];
 		while (cookieChar.charAt(0) === " ") cookieChar = cookieChar.substring(1);
 		if (cookieChar.indexOf(name) === 0) return cookieChar.substring(name.length, cookieChar.length);
@@ -73,27 +73,27 @@ function isPartiallyInViewport(element) {
 
 
 // search for query in array and add css classes to results
-function searchResults(event, objectArray, inputElement, toArray, notResultClasses, minLenght=1) {
+function searchResults(event, objectArray, inputElement, toArray, notResultClasses, minLenght = 1) {
 	var invalidChars = /[^a-z0-9\-]/gi;
 	var query = inputElement.val().toLowerCase();
 	var validQuery = query.replace(invalidChars, "");
-	
+
 	// remove previous added search classes
 	objectArray.forEach((object) => {
-		notResultClasses.forEach(function(resultClass) {
+		notResultClasses.forEach(function (resultClass) {
 			document.getElementById(`movie-${object.movieID}`).classList.remove(resultClass);
 		});
 	});
 
 	if (validQuery.length >= minLenght) {
 		// search for query in array
-		objectArray.filter((object)=>{
-			objectArray = toArray(object).map((attribute)=>{
+		objectArray.filter((object) => {
+			objectArray = toArray(object).map((attribute) => {
 				return attribute.toLowerCase().replace(invalidChars, "");
 			});
-			
+
 			var anyIsTrue = false;
-			objectArray.forEach((attribute)=>{
+			objectArray.forEach((attribute) => {
 				if (attribute.includes(validQuery)) {
 					anyIsTrue = true;
 					return;
@@ -101,19 +101,19 @@ function searchResults(event, objectArray, inputElement, toArray, notResultClass
 			});
 			return !anyIsTrue;
 
-		}).forEach((object)=>{
+		}).forEach((object) => {
 			// add search classes
-			notResultClasses.forEach((resultClass)=>{
+			notResultClasses.forEach((resultClass) => {
 				document.getElementById(`movie-${object.movieID}`).classList.add(resultClass);
 			});
 		});
 	}
 
 	// save search query in url
-	if (event.keyCode === 13 || validQuery.length % ((minLenght+1)*2) === 0) {
+	if (event.keyCode === 13 || validQuery.length % ((minLenght + 1) * 2) === 0) {
 		window.history.pushState({},
 			document.title,
-			`${window.location.origin}${window.location.pathname}?query=${query.replace(/ +/g,"+")}`
+			`${window.location.origin}${window.location.pathname}?query=${query.replace(/ +/g, "+")}`
 		);
 	}
 }
@@ -122,7 +122,7 @@ function searchResults(event, objectArray, inputElement, toArray, notResultClass
 // extend HTML Media Element by the new attribute "playing"
 // https://stackoverflow.com/questions/6877403/how-to-tell-if-a-video-element-is-currently-playing
 Object.defineProperty(HTMLMediaElement.prototype, "playing", {
-	get: function(){
+	get: function () {
 		return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
 	}
 })
@@ -130,44 +130,42 @@ Object.defineProperty(HTMLMediaElement.prototype, "playing", {
 
 // functions for to control the video playback with hotkeys 
 // video actions
-function video_fullscreen(video_wrapper) {
-	var video = $(video_wrapper).children("video").get(0);
-	video_wrapper = $(video_wrapper).get(0);
-	//                                                                note: use default video fullscreen for mobile
-	document.webkitIsFullScreen ? document.webkitExitFullscreen() : ( document.documentElement.clientWidth < 850 ? video.webkitRequestFullscreen() : video.webkitRequestFullscreen() );
+function video_fullscreen(video) {
+	var video = $(video).get(0);
+	video.webkitRequestFullscreen();
 }
-function video_backward(video_wrapper, seconds=10) {
-	var video = $(video_wrapper).children("video").get(0);
+function video_backward(video, seconds = 10) {
+	var video = $(video).get(0);
 	video.currentTime -= seconds;
 }
-function video_play_pause(video_wrapper) {
-	var video = $(video_wrapper).children("video").get(0);
+function video_play_pause(video) {
+	var video = $(video).get(0);
 	if (video.ended) video.load();
 	video.playing ? video.pause() : video.play();
 }
-function video_forward(video_wrapper, seconds=10) {
-	var video = $(video_wrapper).children("video").get(0);
+function video_forward(video, seconds = 10) {
+	var video = $(video).get(0);
 	video.currentTime += seconds;
 }
-function video_volume(video_wrapper) {
-	var video = $(video_wrapper).children("video").get(0);
+function video_volume(video) {
+	var video = $(video).get(0);
 	video.muted ? $(video).prop("muted", false) : $(video).prop("muted", true);
 }
 
 // play specific video in fullscreen
-function playInFullscreen(video_wrapper, delay_ms=0) {
-	setTimeout(()=>{
-		video_fullscreen(video_wrapper);
-		video_play_pause(video_wrapper);
+function playInFullscreen(video, delay_ms = 0) {
+	setTimeout(() => {
+		video_fullscreen(video);
+		video_play_pause(video);
 	}, delay_ms);
 }
 
 
 // on page ready
-$(document).ready(()=>{
+$(document).ready(() => {
 	// remove age restriction classes if age restriction disabled
 	if (getCookie("age_restriction_unlocked") === "true") {
-		$(".age-restricted").each(()=>{
+		$(".age-restricted").each(() => {
 			$(this).removeClass("age-restricted");
 			$(this).removeClass("not-allowed");
 		});
@@ -175,7 +173,7 @@ $(document).ready(()=>{
 
 	// add click events to age restricted elements
 	// query the configured pin and (if correct) disable the age restriction
-	$(".age-restricted").click(()=>{
+	$(".age-restricted").click(() => {
 		var pinInput = prompt("PIN eingeben, um die Altersbeschränkung aufzuheben:");
 		var pinInputHash = CryptoJS.MD5(pinInput).toString();
 		if (pinInputHash === window.ageRestriction_PinHash) {
@@ -189,48 +187,40 @@ $(document).ready(()=>{
 
 	// add events and hotkeys for custom video controls
 	// init video controls and add UI updater
-	$(".video-wrapper").each(function(index, value){
-		var jQ_video_wrapper = $(this);
-		var video = jQ_video_wrapper.children("video").get(0);
-		var click_bindings = "click touch";
-
-		// play video if clicked on video element
-		// note: only if custom controls are applies
-		$(video).bind(`${click_bindings}`, (evt)=>{
-			if (document.documentElement.clientWidth >= 850) video_play_pause(jQ_video_wrapper);
-		});
+	$(".video-wrapper").each(function (index, value) {
+		var video = $(this).children("video").get(0);
 
 		// bind hotkeys to video controls
 		// apply only to first video wrapper
 		if (index === 0) {
-			$(document).bind("keydown", (evt)=>{
+			$(document).bind("keydown", (evt) => {
 				if (isPartiallyInViewport(video)) {
 					switch (evt.originalEvent.key || evt.keyCode) {
 						case 70:
 						case "f":
-							video_fullscreen(jQ_video_wrapper);
+							video_fullscreen(video);
 							break;
 						case 37:
 						case "ArrowLeft":
 						case 74:
 						case "j":
-							video_backward(jQ_video_wrapper);
+							video_backward(video);
 							break;
 						case 32:
 						case " ":
 						case 75:
 						case "k":
-							video_play_pause(jQ_video_wrapper);
+							video_play_pause(video);
 							break;
 						case 39:
 						case "ArrowRight":
 						case 76:
 						case "l":
-							video_forward(jQ_video_wrapper);
+							video_forward(video);
 							break;
 						case 77:
 						case "m":
-							video_volume(jQ_video_wrapper);
+							video_volume(video);
 							break;
 					}
 				}
@@ -239,43 +229,43 @@ $(document).ready(()=>{
 	});
 
 	// add hover and click events for user rating (on movie pages)
-	$(".user.rating").each(function(){
+	$(".user.rating").each(function () {
 		var user_rating_el = $(this);
 
 		// mark selected stars
-		user_rating_el.find(".icon[data-star-action='set']").on("mouseover", (evt)=>{
+		user_rating_el.find(".icon[data-star-action='set']").on("mouseover", (evt) => {
 			var this_star_el = evt.target;
 			var this_star_pos = parseInt($(this_star_el).attr("data-star-position"));
 
-			for (var pos=1; pos<=this_star_pos; pos++) {
+			for (var pos = 1; pos <= this_star_pos; pos++) {
 				var star = user_rating_el.find(`.icon[data-star-position='${pos}']`);
 				star.attr("class", user_rating_el.attr("data-star-filled"));
 			}
-			for (var pos=this_star_pos+1; pos<=5; pos++) {
+			for (var pos = this_star_pos + 1; pos <= 5; pos++) {
 				var star = user_rating_el.find(`.icon[data-star-position='${pos}']`);
 				star.attr("class", user_rating_el.attr("data-star-empty"));
 			}
 		});
-		
+
 		// reset selected stars to loaded state
-		user_rating_el.on("mouseleave", (evt)=>{
-			for (var pos=1; pos<=5; pos++) {
+		user_rating_el.on("mouseleave", (evt) => {
+			for (var pos = 1; pos <= 5; pos++) {
 				var star = user_rating_el.find(`.icon[data-star-position='${pos}'][data-star-action='set']`);
 				star.attr("class", star.attr("data-star-default"));
 			}
 		});
 
 		// send selected stars to the api and if no error occurs, update the stars' default data attribute
-		user_rating_el.find(".icon[data-star-action='set']").on("click touch", (evt)=>{
+		user_rating_el.find(".icon[data-star-action='set']").on("click touch", (evt) => {
 			var this_star_el = evt.target;
 			var this_star_pos = parseInt($(this_star_el).attr("data-star-position"));
 			var url = `${window.location.pathname}user-rating/set/${this_star_pos}`;
-			
+
 			$.ajax({
 				url: url,
-				error: ()=>{ alert("Failed to save user rating."); },
-				success: ()=>{
-					for (var pos=1; pos<=5; pos++) {
+				error: () => { alert("Failed to save user rating."); },
+				success: () => {
+					for (var pos = 1; pos <= 5; pos++) {
 						var star = user_rating_el.find(`.icon[data-star-position='${pos}'][data-star-action='set']`);
 						star.attr("data-star-default", star.attr("class"));
 					}
@@ -284,14 +274,14 @@ $(document).ready(()=>{
 		});
 
 		// reset user rating (send 0 stars to the api) and if no error occurs, update the stars' default data attribute
-		user_rating_el.find(".icon[data-star-action='reset']").on("click touch", (evt)=>{
+		user_rating_el.find(".icon[data-star-action='reset']").on("click touch", (evt) => {
 			var url = `${window.location.pathname}user-rating/set/0`;
-			
+
 			$.ajax({
 				url: url,
-				error: ()=>{ alert("Failed to reset user rating."); },
-				success: ()=>{
-					for (var pos=1; pos<=5; pos++) {
+				error: () => { alert("Failed to reset user rating."); },
+				success: () => {
+					for (var pos = 1; pos <= 5; pos++) {
 						var star = user_rating_el.find(`.icon[data-star-position='${pos}'][data-star-action='set']`);
 						star.attr("data-star-default", user_rating_el.attr("data-star-empty"));
 						user_rating_el.trigger("mouseleave");
